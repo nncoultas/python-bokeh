5fed57d74d619a3fb08a1a8ef9a89ebc2a98bd70
Nick Coultas <nncoultas@yahoo.com>
2018-06-20 10:48:49 -0700

    added graph classes

---
+++ src/graph.py
@@ -0,0 +1,27 @@
+class Edge:
+   def __init__(self, destination):
+       self.destination = destination
+
+
+class Vertex:
+    def  __init__(self, value,  **pos):
+        self.value = value
+        self.color = 'white'
+        self.pos = pos
+        self.edges = []
+
+class Graph:
+    def  __init__(self):
+        self.vertexes = []
+
+
+    def debug_create_test_data(self):
+        debug_vertex_1 = Vertex('t1', x=40, y=40)
+        debug_vertex_2 = Vertex('t2', x=80, y=80)
+        debug_vertex_3 = Vertex('t3', x=40, y=80)
+
+        debug_edge_1 = Edge(debug_vertex_2)
+        debug_vertex_1.edges.append(debug_edge_1)
+
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+
